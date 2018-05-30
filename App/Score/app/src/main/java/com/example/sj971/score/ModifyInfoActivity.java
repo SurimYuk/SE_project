71214246ff9606c0415742bc01b356be234c5c45
SuJeong2 <sj971020@naver.com>
2018-05-30 10:12:45 +0900

    2018-05

--- App/Score/app/src/main/java/com/example/sj971/score/ModifyInfoActivity.java
+++ App/Score/app/src/main/java/com/example/sj971/score/ModifyInfoActivity.java
@@ -1,15 +1,26 @@
 package com.example.sj971.score;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class ModifyInfoActivity extends AppCompatActivity {
+
+    EditText numberEdit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_modify_info);
 
+        numberEdit = (EditText) findViewById(R.id.number);
 
+        Intent intent = getIntent();
+        Bundle bundle = new Bundle();
+        bundle = intent.getExtras();
+        String value = bundle.getString("Number");
+
+        numberEdit.setText(value);
     }
 }
