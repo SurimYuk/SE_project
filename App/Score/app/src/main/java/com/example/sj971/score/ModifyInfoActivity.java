ff49dee4b0c37eb4fd093628a787a10f5e3c41c0
SuJeong2 <sj971020@naver.com>
2018-05-30 10:40:01 +0900

    2018-05

--- App/Score/app/src/main/java/com/example/sj971/score/ModifyInfoActivity.java
+++ App/Score/app/src/main/java/com/example/sj971/score/ModifyInfoActivity.java
@@ -3,17 +3,20 @@ package com.example.sj971.score;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class ModifyInfoActivity extends AppCompatActivity {
 
     EditText numberEdit;
+    Button storeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_modify_info);
 
+        storeButton=(Button)findViewById(R.id.store);
         numberEdit = (EditText) findViewById(R.id.number);
 
         Intent intent = getIntent();
